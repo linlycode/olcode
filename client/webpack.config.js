edff7539d68fcbfb86fef222497f6641eb3183eb
linly <linlycode@gmail.com>
2018-03-19 13:52:09 +0800

    Fix webpack config lint errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,32 +4,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  mode: 'development',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "initial",
-    },
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({ template: './index.html' }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  devServer: {
-    // contentBase: './dist',
-    hot: true,
-  }
+	entry: {
+		app: './src/index.js',
+	},
+	mode: 'development',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+		],
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'initial',
+		},
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({ template: './index.html' }),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	devServer: {
+		// contentBase: './dist',
+		hot: true,
+	},
 }
