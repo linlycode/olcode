e54956ed15a5ac22c1321c6ff382e634a86e9df0
linlycode <linlycode@gmail.com>
2018-07-19 00:41:03 +0800

    Indent using tabs, update editorconfig and tslint config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome</h1>
-        </header>
-        <p className="App-intro">
-          Hello
-        </p>
-      </div>
-    );
-  }
+	public render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Welcome</h1>
+				</header>
+				<p className="App-intro">
+					Hello
+				</p>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
