6e7fbac922c9f038a386129014d9e9934d6fe362
linly <linlycode@gmail.com>
2018-07-24 20:46:58 +0800

    Resolves #8 use real ID generator (#18)
    * Resolves #8 use real ID generator
    
    * Add comments to export IDGenerator and its method

--- pkg/hubpkg/peer.go
+++ pkg/hubpkg/peer.go
@@ -6,21 +6,18 @@ import (
 	"github.com/linlycode/olcode/pkg/idgen"
 )
 
+var peerIDGen = &idgen.IDGenerator{}
+
 // Peer denotes the peer entity in a hub
 type Peer struct {
 	ID     int64
 	sender io.Writer
 }
 
-// NewPeerID is used to generate unique peerID
-func NewPeerID() (int64, error) {
-	return idgen.GetIDGen().GenID(idgen.PeerID)
-}
-
 // NewPeer create a peer given id
-func NewPeer(id int64, sender io.Writer) *Peer {
+func NewPeer(sender io.Writer) *Peer {
 	return &Peer{
-		ID:     id,
+		ID:     peerIDGen.GenerateID(),
 		sender: sender,
 	}
 }
