458667b3a4ffd1d30304a73ddadeb5648ce2c08f
WEI Xikai <ShiKaiWi@users.noreply.github.com>
2018-07-31 23:55:38 +0800

    add webrtc-adapter (#31)

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,7 +1,7 @@
 import { RouterAPI } from 'dva'
 import { Route, Router, Switch } from 'dva/router'
 import * as React from 'react'
-import HomePage from './pages/HomePage/index'
+import HomePage from './pages/HomePage'
 
 
 function RouterConfig({ history }: RouterAPI) {
